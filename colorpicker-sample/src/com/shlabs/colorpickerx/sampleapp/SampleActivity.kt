db87deaf2090263e2df543f375bbd1191b84b33a
Saul Henriquez <saul_henriquez@hotmail.com>
2022-06-17 15:03:15 -0500

    Rewrite Sample App using Compose
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- colorpicker-sample/src/com/shlabs/colorpickerx/sampleapp/SampleActivity.kt
+++ colorpicker-sample/src/com/shlabs/colorpickerx/sampleapp/SampleActivity.kt
@@ -1,6 +1,6 @@
 /*
  *  This file is part of ColorPickerX
- *  Copyright (c) 2021   Saul Henriquez
+ *  Copyright (c) 2022   Saul Henriquez
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as
@@ -18,11 +18,29 @@
 
 package com.shlabs.colorpickerx.sampleapp
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.insets.ui.LocalScaffoldPadding
 import com.shlabs.colorpickerx.ColorPicker
 import com.shlabs.colorpickerx.ColorPickerTab
 import com.shlabs.colorpickerx.ColorSelectorPresets
@@ -32,140 +50,227 @@ class SampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
 
-        val dialogButton = findViewById<MaterialButton>(R.id.dialog_button)
-        dialogButton.setOnClickListener {
-            val colorPicker = ColorPicker(this@SampleActivity)
-            val colors = ArrayList<String>()
-            colors.add("#FFF44336")
-            colors.add("#FFE91E63")
-            colors.add("#FFFF2C93")
-            colors.add("#FF9C27B0")
-            colors.add("#FF673AB7")
-            colors.add("#FF3F51B5")
-            colors.add("#FF2196F3")
-            colors.add("#FF03A9F4")
-            colors.add("#FF00BCD4")
-            colors.add("#FF009688")
-            colors.add("#FF4CAF50") // GREEN 500
-            colors.add("#FF8BC34A") // LIGHT GREEN 500
-            colors.add("#FFCDDC39") // LIME 500
-            colors.add("#FFFFEB3B") // YELLOW 500
-            colors.add("#FFFFC107") // AMBER 500
-            colors.add("#FFFF9800") // ORANGE 500
-            colors.add("#FF795548") // BROWN 500
-            colors.add("#FF607D8B") // BLUE GREY 500
-            colors.add("#FF9E9E9E") // GREY 500
-            colorPicker
-                .setDefaultColorButton(Color.parseColor("#FFF44336"))
-                .setColors(colors)
-                .setColumns(5)
-                .setRoundColorButton(true)
-                .setOnChooseColorListener(object : OnChooseColorListener {
-                    override fun onChooseColor(position: Int, color: Int) {
-                        Log.d("position", "" + position)
-                    }
-
-                    override fun onCancel() {}
-                })
-                //.addListenerButton("newButton") { v, position, color -> Log.d("position", "" + position) }.show()
-                .show()
-        }
-
-        val dialogButton2 = findViewById<MaterialButton>(R.id.dialog_button_square)
-        dialogButton2.setOnClickListener {
-            val colorPicker = ColorPicker(this@SampleActivity)
-            val colors = ArrayList<String>()
-            colors.add("#FFF44336")
-            colors.add("#FFE91E63")
-            colors.add("#FFFF2C93")
-            colors.add("#FF9C27B0")
-            colors.add("#FF673AB7")
-            colors.add("#FF3F51B5")
-            colors.add("#FF2196F3")
-            colors.add("#FF03A9F4")
-            colors.add("#FF00BCD4")
-            colors.add("#FF009688")
-            colors.add("#FF4CAF50") // GREEN 500
-            colors.add("#FF8BC34A") // LIGHT GREEN 500
-            colors.add("#FFCDDC39") // LIME 500
-            colors.add("#FFFFEB3B") // YELLOW 500
-            colors.add("#FFFFC107") // AMBER 500
-            colors.add("#FFFF9800") // ORANGE 500
-            colors.add("#FF795548") // BROWN 500
-            colors.add("#FF607D8B") // BLUE GREY 500
-            colors.add("#FF9E9E9E") // GREY 500
-            colorPicker
-                .setDefaultColorButton(Color.parseColor("#FFF44336"))
-                .setColors(colors)
-                .setColumns(5)
-                .setOnChooseColorListener(object : OnChooseColorListener {
-                    override fun onChooseColor(position: Int, color: Int) {
-                        Log.d("position", "" + position)
-                    }
-
-                    override fun onCancel() {}
-                })
-                //.addListenerButton("newButton") { v, position, color -> Log.d("position", "" + position) }.show()
-                .show()
-        }
-
-        val tabbedButton = findViewById<MaterialButton>(R.id.tab_dialog_button)
-        tabbedButton.setOnClickListener {
-            val colorPicker = ColorPickerTab(this@SampleActivity)
-            colorPicker.showAlpha(true)
-            val colors = ArrayList<String>()
-            colors.add("#FFF44336")
-            colors.add("#FFE91E63")
-            colors.add("#FFFF2C93")
-            colors.add("#FF9C27B0")
-            colors.add("#FF673AB7")
-            colors.add("#FF3F51B5")
-            colors.add("#FF2196F3")
-            colors.add("#FF03A9F4")
-            colors.add("#FF00BCD4")
-            colors.add("#FF009688")
-            colors.add("#FF4CAF50") // GREEN 500
-            colors.add("#FF8BC34A") // LIGHT GREEN 500
-            colors.add("#FFCDDC39") // LIME 500
-            colors.add("#FFFFEB3B") // YELLOW 500
-            colors.add("#FFFFC107") // AMBER 500
-            colors.add("#FFFF9800") // ORANGE 500
-            colors.add("#FF795548") // BROWN 500
-            colors.add("#FF607D8B") // BLUE GREY 500
-            colors.add("#FF9E9E9E") // GREY 500
-            colorPicker
-                .setDefaultColorButton(Color.WHITE)
-                .setColors(colors)
-                .setColumns(5)
-                .setOnChooseColorListener(object : OnChooseColorListener {
-                    override fun onChooseColor(position: Int, color: Int) {
-                        Log.d("position", "" + color)
-                    }
-
-                    override fun onCancel() {}
-                })
-                .show()
-        }
-
-        val buttonCustom = findViewById<MaterialButton>(R.id.custom_button)
-        buttonCustom.setOnClickListener {
-            val colorPicker = ColorSelectorPresets(this@SampleActivity)
-
-            colorPicker
-                .setDefaultColorButton(Color.RED)
-                .setColumns(4)
-                .setColorButtonSize(48, 48)
-                .setRoundColorButton(true)
-                .setOnChooseColorListener(object : OnChooseColorListener {
-                    override fun onChooseColor(position: Int, color: Int) {
-                        Log.d("position", "" + color)
-                    }
-
-                    override fun onCancel() {}
-                })
-                .show()
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+
+    val searchBarHeight = 56.dp
+    val searchBarVerticalMargin = 8.dp
+    val innerPadding = remember { MutablePaddingValues() }
+    val statusBarHeight = 26.dp
+    val contentShift = statusBarHeight + searchBarVerticalMargin + searchBarHeight / 2
+
+    Scaffold(
+    ) {
+        val layoutDirection = LocalLayoutDirection.current
+        innerPadding.left = it.calculateLeftPadding(layoutDirection)
+        innerPadding.top = it.calculateTopPadding() - contentShift
+        innerPadding.right = it.calculateRightPadding(layoutDirection)
+        innerPadding.bottom = it.calculateBottomPadding()
+        val context = LocalContext.current
+
+        val colors: ArrayList<String> = arrayListOf()
+        colors.addAll(context.resources.getStringArray(R.array.dialog_colors))
+
+        CompositionLocalProvider(
+            LocalScaffoldPadding provides innerPadding
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+                Spacer(modifier = Modifier.height(16.dp))
+                //Button 1
+                Button(
+                    onClick = {
+                        val colorPicker = ColorPicker(context)
+                            .setDefaultColorButton(android.graphics.Color.parseColor(colors[1]))
+                            .setColors(colors)
+                            .setColumns(5)
+                            .setRoundColorButton(true)
+                            .setOnChooseColorListener(object : OnChooseColorListener {
+                                override fun onChooseColor(position: Int, color: Int) {
+                                    Log.d("position", "" + position)
+                                }
+
+                                override fun onCancel() {}
+                            })
+                        colorPicker.show()
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(220.dp)
+                ) {
+                    Image(
+                        painterResource(id = R.drawable.ic_email_unread),
+                        contentDescription = stringResource(id = R.string.dialog_button_rounded),
+                        modifier = Modifier.size(36.dp),
+                        colorFilter = ColorFilter.tint(
+                            Color(0xFFFFFFFF)
+                        )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.dialog_button_rounded),
+                        modifier = Modifier.padding(start = 8.dp),
+                        fontSize = 18.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                //Button 2
+                Button(
+                    onClick = {
+                        val colorPicker = ColorPicker(context)
+                            .setDefaultColorButton(android.graphics.Color.parseColor(colors[1]))
+                            .setColors(colors)
+                            .setColumns(5)
+                            .setOnChooseColorListener(object : OnChooseColorListener {
+                                override fun onChooseColor(position: Int, color: Int) {
+                                    Log.d("position", "" + position)
+                                }
+
+                                override fun onCancel() {}
+                            })
+                        colorPicker.show()
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(220.dp)
+                ) {
+                    Image(
+                        painterResource(id = R.drawable.ic_email_unread),
+                        contentDescription = stringResource(id = R.string.dialog_button_square),
+                        modifier = Modifier.size(36.dp),
+                        colorFilter = ColorFilter.tint(
+                            Color(0xFFFFFFFF)
+                        )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.dialog_button_square),
+                        modifier = Modifier.padding(start = 8.dp),
+                        fontSize = 18.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                //Button 3
+                Button(
+                    onClick = {
+                        val colorPicker = ColorPickerTab(context)
+                        colorPicker
+                            .setDefaultColorButton(android.graphics.Color.WHITE)
+                            .setColors(colors)
+                            .setColumns(5)
+                            .showAlpha(true)
+                            .setOnChooseColorListener(object : OnChooseColorListener {
+                                override fun onChooseColor(position: Int, color: Int) {
+                                    Log.d("position", "" + color)
+                                }
+
+                                override fun onCancel() {}
+                            })
+                            .show()
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(220.dp)
+                ) {
+                    Image(
+                        painterResource(id = R.drawable.ic_email),
+                        contentDescription = stringResource(id = R.string.tab_button),
+                        modifier = Modifier.size(36.dp),
+                        colorFilter = ColorFilter.tint(
+                            Color(0xFFFFFFFF)
+                        )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.tab_button),
+                        modifier = Modifier.padding(start = 8.dp),
+                        fontSize = 18.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                //Button 4
+                Button(
+                    onClick = {
+                        val colorPicker = ColorSelectorPresets(context)
+                        colorPicker
+                            .setDefaultColorButton(android.graphics.Color.RED)
+                            .setColumns(4)
+                            .setColorButtonSize(48, 48)
+                            .setRoundColorButton(true)
+                            .setOnChooseColorListener(object : OnChooseColorListener {
+                                override fun onChooseColor(position: Int, color: Int) {
+                                    Log.d("position", "" + color)
+                                }
+
+                                override fun onCancel() {}
+                            })
+                            .show()
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(220.dp)
+                ) {
+                    Image(
+                        painterResource(id = R.drawable.ic_email),
+                        contentDescription = stringResource(id = R.string.custom_buttom),
+                        modifier = Modifier.size(36.dp),
+                        colorFilter = ColorFilter.tint(
+                            Color(0xFFFFFFFF)
+                        )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.custom_buttom),
+                        modifier = Modifier.padding(start = 8.dp),
+                        fontSize = 18.sp,
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MainScreen()
+}
+
+@Stable
+internal class MutablePaddingValues : PaddingValues {
+    var left: Dp by mutableStateOf(0.dp)
+    var top: Dp by mutableStateOf(0.dp)
+    var right: Dp by mutableStateOf(0.dp)
+    var bottom: Dp by mutableStateOf(0.dp)
+
+    override fun calculateLeftPadding(layoutDirection: LayoutDirection) = left
+
+    override fun calculateTopPadding(): Dp = top
+
+    override fun calculateRightPadding(layoutDirection: LayoutDirection) = right
+
+    override fun calculateBottomPadding(): Dp = bottom
 }
